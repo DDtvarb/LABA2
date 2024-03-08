903bb8034d077d8eea587c044b042b524325c45b
DDtvarb <153864225+DDtvarb@users.noreply.github.com>
2024-03-08 21:31:18 +0300

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include <ctime>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a[10];
+	int i;
+	int* p = a;
+	int n = 0;
+	srand(time(0));
+	for (i = 0; i < 10; i++) {
+		*(p + i) = 1 + rand() % 100;
+		cout << *(p + i) << "\n";
+		if (*(p + i) % 3 == 0) {
+
+			n = n + 1;
+		}
+	}
+	cout << "=================" << endl;
+	int k = 0;
+	for (i = 0; i < 10; i++) {
+		if (*(p + i) % 3 == 0) {
+			k = i;
+
+		}
+	
+	}
+	cout << "ИНДЕКС ПОСЛЕДНЕГО ЭЛЕМЕНТА КРАТНОГО 3:" << k << endl;
+	
+
+	if (n == 0) {
+		cout << "ЧИСЛА, КОТОРЫЕ ДЕЛЯТСЯ НА 3 ОТСУТСТВУЮТ";
+	}
+	else {
+
+		cout << "ИХ КОЛИЧЕСТВО РАВНО:" << n << endl;
+	}
+
+}
---
+++ 2.2.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include <ctime>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int i;
+	int z;
+	cout << "Введите кол-во элементов массива: ";
+	cin >> z;
+	if (cin.fail()) {
+		cout << "Введите корректное колличество элементов"<<endl<<endl;
+		exit(0);
+	}
+	if (z<0 or z==0) {
+		cout << "Введите корректное колличество элементов" << endl << endl;
+		exit(0);
+	}
+	int* a = new int[z];
+	int pr = 1;
+	int* p = a;
+	int n = 0;
+	int k = 0;
+	int s = 0;
+	srand(time(0));
+	for (i = 0; i < z; i++) {
+		*(p + i) = 1 + rand() % 20 - 10;
+		cout <<*(p + i) << "\n";
+		if (*(p + i) < 0){
+			pr = pr * *(p + i);
+		}
+		
+		if (n <= *(p + i)) {
+			n = *(p + i);
+			k = i;
+		}
+	}	
+	for (int l = 0; l != k; l++) {
+		if (*(p + l) > 0) {
+			s = s + *(p + l);
+		}
+	}
+	cout << "Преобразованный массив:" << endl;
+	for (i = z - 1; i >= 0; i--) {
+		cout << *(p + i) << endl;
+	}	
+	cout << "Произведение отрицательных элементов массива: " <<pr << endl;
+	cout << "Максимальный элемент массива: " << n << endl;
+	cout << "Индекс максимального элемента массива: " << k << endl;
+	cout << "Сумма положительных элементов массива, расспаложенных до максимального: " << s << endl;
+}
